12ddb3803a5fdd010cd8c38ee9a4a7aa2c6788be
Vivek Goel <vivekgoel@Viveks-Air.lan1>
2018-12-02 23:27:09 -0600

    Fixed issue with 06 file

--- plaintextmaker.py
+++ plaintextmaker.py
@@ -1,9 +1,9 @@
-
 import nltk
 stop = nltk.corpus.stopwords.words("english")
 def readFile(filename):
 	ret = []
-	lines = [line.rstrip('\n') for line in open(filename)]
+	file = open(filename, encoding="utf8", errors='ignore')
+	lines = [line.rstrip('\n') for line in file]
 	for line in lines:
 		words = line.split()
 		if len(words) > 1:
@@ -11,6 +11,5 @@ def readFile(filename):
 			if append not in stop and not words[0].startswith("@"):
 				ret.append(append)
 	return " ".join(ret)
-print(readFile("./wordLemPoS/news_1879_742950.txt"))
-print(readFile("./wordLemPoS/news_1880_743450.txt"))
-
+print(readFile("./news_1985_671550.txt"))
+print(readFile("./fic_2006_51066.txt"))
